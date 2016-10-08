3024b1a7651d2cf5054b8e15a9ab4fd4295de564
ytixu <yi.t.xu@mail.mcgill.ca>
2016-10-08 13:57:26 -0400

    Update construct.py

--- src/construct.py
+++ src/construct.py
@@ -1,5 +1,4 @@
 import csv
-import pickle
 import networkx as nx
 
 
@@ -15,8 +14,9 @@ def buildFromNodeList(file_name, setting, G):
 		id_index = -1
 		for i, row in enumerate(spamreader):
 			if i == 0:
-				lowered_keys = map(lower, row)
-				attr_indices = {index(x, lowered_keys): x for x in intersect(setting.node_attributes, lowered_keys)}
+				lowered_keys = map(lambda x: x.lower(), row)
+				intersect_keys = list(set(setting.node_attributes).intersection(lowered_keys))
+				attr_indices = {index(x, lowered_keys): x for x in intersect_keys}
 				id_index = index(setting.id, lowered_keys)
 
 				if id_index < 0:
@@ -39,8 +39,9 @@ def buildFromEdgeList(file_name, setting, G):
 		target_index = -1
 		for i, row in enumerate(spamreader):
 			if i == 0:
-				lowered_keys = map(lower, row)
-				attr_indices = {index(x, lowered_keys): x for x in intersect(setting.edge_attributes, lowered_keys)}
+				lowered_keys = map(lambda x: x.lower(), row)
+				intersect_keys = list(set(setting.edge_attributes).intersection(lowered_keys))
+				attr_indices = {index(x, lowered_keys): x for x in intersect_keys}
 				source_index = index(setting.source, lowered_keys)
 				source_index = index(setting.target, lowered_keys)
 
@@ -76,6 +77,14 @@ def buildDirectedGraph(edge_list, node_list, setting):
 	return buildGraph(edge_list, node_list, setting, True)
 
 
-def dumpObject(G, file_name):
+def dumpGraph(G, file_name):
+	nx.write_gpickle(G, file_name)
+	
+def loadGraph(file_name):
+	return nx.read_gpickle(file_name)
 
 def dumpToGephi(G, file_name):
+	nx.write_gexf(G, file_name)
+	
+def loadFromGephi(file_name):
+	return nx.read_gexf(file_name)
